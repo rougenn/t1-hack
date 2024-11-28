fa1822c4b75fd783ddb6e0670386eea76037768a
rougenn <deziq.hi@gmail.com>
2024-11-28 11:15:47 +0300

    stable ver

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -2,32 +2,22 @@ package main
 
 import (
 	"log"
-	"os"
-	"path/filepath"
 	"t1/internal/pkg/db"
 	"t1/internal/pkg/server"
 )
 
 func main() {
-	// Печатаем текущую рабочую директорию
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatalf("Failed to get working directory: %v", err)
-	}
-	log.Printf("Current working directory: %s", dir)
-
 	// Формируем относительный путь к файлу миграции
-	migrationFilePath := filepath.Join("scripts", "migration.sql")
+	migrationFilePath := "scripts/migration.sql"
 
 	// Запуск сервера
 	s := server.New(":8090")
 
 	// Запускаем миграцию
-	err = db.Migrate(s.DB, migrationFilePath)
+	err := db.Migrate(s.DB, migrationFilePath)
 	if err != nil {
 		log.Fatalf("Migration failed: %v", err)
 	}
 
-	// Запуск сервера
 	s.Start()
 }
